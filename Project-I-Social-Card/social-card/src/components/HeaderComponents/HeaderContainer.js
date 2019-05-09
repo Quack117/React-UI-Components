a9ce46d8cd1b1b12c2c921680e13e72fb77777fc
Ben Q <Quackenbush117@gmail.com>
2019-05-09 00:05:43 -0600

    MVP, got busy and forgot to commit earlier

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import HeaderImage from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+const HeaderContainer = () => {
+    return (
+    <div className ="headerContainer">
+        <HeaderImage/>
+        <HeaderContent/>
+        
+    </div>
+    ); 
+}
+
+export default HeaderContainer;
